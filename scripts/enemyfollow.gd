21ed4b86e64423540351981f4db0eb6688bdc057
SunnyBunns-3221 <charliesun567@gmail.com>
2025-09-01 16:11:09 -0400

    issues

--- scripts/enemyfollow.gd
+++ scripts/enemyfollow.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 @export var speed: float = 50.0
-@export var stop_time: float = 0.5  
-@onready var player: Node2D = get_parent().get_node("player")
+@export var stop_time: float = 0.5
+@export var stop_distance: float = 20.0
+@onready var player: Node2D = get_parent().get_node("Player")
 
 var stop_timer: float = 0.0
 
@@ -10,20 +11,24 @@ func _physics_process(delta: float) -> void:
 	if not player:
 		return
 
-	
 	if stop_timer > 0.0:
 		stop_timer -= delta
 		velocity = Vector2.ZERO
 		move_and_slide()
 		return
 
-	var direction = (player.global_position - global_position).normalized()
-	velocity = direction * speed
+	var to_player = player.global_position - global_position
+	var distance = to_player.length()
 
-	
-	var collision = move_and_collide(velocity * delta)
-	if collision and collision.get_collider() == player:
-		velocity = Vector2.ZERO
-		stop_timer = stop_time 
+	if distance > stop_distance:
+		var direction = to_player.normalized()
+		velocity = direction * speed
+
+		var collision = move_and_collide(velocity * delta)
+		if collision and collision.get_collider() == player:
+			velocity = Vector2.ZERO
+			stop_timer = stop_time
 	else:
-		move_and_slide()
+		velocity = Vector2.ZERO
+
+	move_and_slide()
